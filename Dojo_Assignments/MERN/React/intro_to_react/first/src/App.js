cee3b593a9bf985c2192ee034f3ee8681d52be6b
Timothy Larramore <tlarramore@gmail.com>
2022-08-30 19:39:20 -0400

    7:38 8/30 commit

---
+++ Dojo_Assignments/MERN/React/intro_to_react/first/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {    
+      return (
+        <>            
+          <h1>Hello Dojo</h1>            
+          <ul>Things I need to do:</ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+
+        </>        
+      );    
+  }
+
+
+export default App;
