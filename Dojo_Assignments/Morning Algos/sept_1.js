041746da5f0624f9bf8bf4935ec387894cf0f0cd
Timothy Larramore <tlarramore@gmail.com>
2022-09-01 13:38:13 -0400

    9/1 1:37

---
+++ Dojo_Assignments/Morning Algos/sept_1.js
@@ -0,0 +1,74 @@
+// merge
+const sortedA1 = [];
+const sortedB1 = [];
+const expectedMerge1 = [];
+
+const sortedA2 = [5];
+const sortedB2 = [2];
+const expectedMerge2 = [2, 5];
+
+const sortedA3 = [3];
+const sortedB3 = [2, 3, 4, 7];
+const expectedMerge3 = [2, 3, 3, 4, 7];
+
+const sortedA4 = [1, 2, 4, 5, 6, 9];
+const sortedB4 = [3, 7, 8, 10];
+const expectedMerge4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Efficiently merges two already sorted arrays into a new sorted array.
+ * Do not mutate the given arrays.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} left
+ * @param {Array<number>} right
+ * @returns {Array<number>} A new sorted array containing all the elements of
+ *    both given halves.
+ */
+// arr is the new combined array?
+
+function merge(left = [], right = []) {
+  let arr = []
+    // Break out of loop if any one of the array gets empty
+    while (left.length && right.length) {
+        // Pick the smaller among the smallest element of left and right sub arrays 
+        if (left[0] < right[0]) {
+            arr.push(left.shift())  
+        } else {
+            arr.push(right.shift()) 
+        }
+    }
+    
+    // Concatenating the leftover elements
+    // (in case we didn't go through the entire left or right array)
+    return [ ...arr, ...left, ...right ]
+}
+
+console.log(merge(sortedA1,sortedB1))
+console.log(merge(sortedA2,sortedB2))
+console.log(merge(sortedA3,sortedB3))
+console.log(merge(sortedA4,sortedB4))
+
+
+// mergeSort
+const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const expectedSort = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Creates a new sorted array based on the given nums being recursively split
+ * and merged.
+ * Best: O(n log(n)) linearithmic.
+ * Avg: O(n log(n)) linearithmic.
+ * Worst: O(n log(n)) linearithmic.
+ * @param {Array<number>} nums
+ * @returns {Array<number>} A New sorted array.
+ */
+// Merge two subarrays L and M into arr
+
+
+console.log(mergeSort(numsOrdered));
+console.log(mergeSort(numsRandomOrder));
+console.log(mergeSort(numsReversed));
+console.log(mergeSort(expected));
