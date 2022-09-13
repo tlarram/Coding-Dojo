be2732892c3394d3e9fc0bb993efe12d10175fb8
Timothy Larramore <tlarramore@gmail.com>
2022-09-13 00:38:16 -0400

    fixed a bug

--- Dojo_Assignments/MERN/FullStackMern/projectManager/client/src/views/AllProducts.jsx
+++ Dojo_Assignments/MERN/FullStackMern/projectManager/client/src/views/AllProducts.jsx
@@ -3,12 +3,20 @@ import axios from 'axios'
 import{Link} from 'react-router-dom'
 
 const AllProducts = (props) => {
+    const handleDelete= (deleteId)=>{
+        axios.delete(`http://localhost:8000/api/product/${deleteId}`)
+        .then(res=>{
+            props.onDelete(deleteId)
 
+        })
+        .catch(err=>console.log(err))
+
+    }
   return (
     <div>
         <h1>All Products</h1>
         {props.products.map((product,i) =>
-        <p key={i}><Link to={`/product/${product._id}`}>{product.title}</Link><Link to={`/product/edit/${product._id}`}>Edit</Link></p>
+        <p key={i}><Link to={`/product/${product._id}`}>{product.title}</Link>   <Link to={`/product/edit/${product._id}`}>Edit</Link>  <button onClick={()=>handleDelete(product._id)}>Delete</button></p>
 
         )}
             {/* {props.products.map((eachProduct,i) => {
